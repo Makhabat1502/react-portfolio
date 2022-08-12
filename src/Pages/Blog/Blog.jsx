02eb0cc4b39b77c9b18903d702cd43f43532480a
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2022-08-12 12:07:54 +0600

    Add pagination to the Blog page

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Posts from "../../components/Posts/Posts";
 import axios from "axios";
+import { Pagination } from "../../components";
 
 function Blog() {
   const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
 
   useEffect(() => {
     const getPosts = async () => {
@@ -17,11 +20,24 @@ function Blog() {
     getPosts();
   }, []);
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPage = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPage, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <section className="blog">
       <h1 style={{ textAlign: "center" }}>My Blog</h1>
       <div className="container">
-        <Posts posts={posts} />
+        <Posts posts={currentPosts} />
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        />
       </div>
     </section>
   );
