ad936e17105a587b24335b01a75e9ca5f0005718
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2022-08-22 16:15:50 +0600

    Add form to Contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import "./contact.scss";
 
 function Contact() {
   return (
     <section className="contact">
-      <h1>Contact page</h1>
+      <h1>My Contacts</h1>
+      <div className="container contacts__container">
+        <div className="contact__col">
+          <form>
+            <label>
+              Your Name
+              <input placeholder="John Doe" />
+            </label>
+            <div className="contact__error">
+              <p></p>
+            </div>
+            <label>
+              Your Email
+              <input placeholder="test@gmail.com" />
+            </label>
+            <div className="contact__error">
+              <p></p>
+            </div>
+            <label>
+              Your Message
+              <textarea></textarea>
+            </label>
+            <div className="contact__error">
+              <p></p>
+            </div>
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
